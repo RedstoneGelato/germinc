30cdc6877b16ddd06b3c73442f9d8feb286812ce
RedstoneGelato <wanjerold@gmail.com>
2026-02-19 16:55:49 +1100

    no work

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ class FrameGrabber(threading.Thread):
         self.cap = picamera2.Picamera2()
         cfg = self.cap.create_video_configuration()
         self.cap.configure(cfg)
-        self.cap.set_controls({"AwbEnable": True}, {"AwbMode": AwbModeEnum.Auto}, {"AnalogueGain": 1.0})
+        self.cap.set_controls({"AwbEnable": True}, {"AwbMode": 1}, {"AnalogueGain": 1.0})
         self.cap.start()
 
     def run(self):
