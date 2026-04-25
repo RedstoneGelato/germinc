f1d07e450ec5d2279fedb5602026e197fa70528a
RedstoneGelato <wanjerold@gmail.com>
2026-04-25 20:03:36 +1000

    this should work

--- main.py
+++ main.py
@@ -393,12 +393,18 @@ def main():
 
             xvel = desired_pos[0]
             yvel = desired_pos[1]
+            angle = -compass
+            x_field = yvel
+            y_field = -xvel
+            x_robot = x_field * math.cos(angle) - y_field * math.sin(angle)
+            y_robot = x_field * math.sin(angle) + y_field * math.cos(angle)
+
             heading_error = desired_heading - compass
             heading_error = (heading_error + math.pi) % (2 * math.pi) - math.pi
             heading_error = round(heading_error, 3)
             rot = spin_weight * heading_error
             rot = max(min(rot, 1), -1)
-            motors.motorspeed1,motors.motorspeed2,motors.motorspeed3,motors.motorspeed4 = VelocityToMotor(xvel,yvel,rot,maxspd)
+            motors.motorspeed1,motors.motorspeed2,motors.motorspeed3,motors.motorspeed4 = VelocityToMotor(x_robot,y_robot,rot,maxspd)
             kick()
     
     except KeyboardInterrupt:
