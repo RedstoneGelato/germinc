2e6f0a4dd04bbbfcaa21944efcdde3121cf1b225
RedstoneGelato <wanjerold@gmail.com>
2026-06-25 19:07:36 +1000

    deleted some redundant lines

--- main.py
+++ main.py
@@ -169,7 +169,6 @@ class MotorThread(threading.Thread):
         self.motor1 = PowerfulBLDCDriver(self.i2c, 26)
         self.motor1.set_current_limit_foc(262144)  # max 8 amps is 524288
         self.motor1.set_id_pid_constants(1500, 200)
-        self.motor1.set_iq_pid_constants(1500, 200)
         self.motor1.set_speed_pid_constants(4e-2, 4e-4, 3e-2)
         self.motor1.set_position_pid_constants(275, 0, 0)
         self.motor1.set_position_region_boundary(250000)
@@ -181,7 +180,6 @@ class MotorThread(threading.Thread):
         self.motor2 = PowerfulBLDCDriver(self.i2c, 28)
         self.motor2.set_current_limit_foc(262144)  # 4 amps
         self.motor2.set_id_pid_constants(1500, 200)
-        self.motor2.set_iq_pid_constants(1500, 200)
         self.motor2.set_speed_pid_constants(4e-2, 4e-4, 3e-2)
         self.motor2.set_position_pid_constants(275, 0, 0)
         self.motor2.set_position_region_boundary(250000)
@@ -193,7 +191,6 @@ class MotorThread(threading.Thread):
         self.motor3 = PowerfulBLDCDriver(self.i2c, 27)
         self.motor3.set_current_limit_foc(262144)
         self.motor3.set_id_pid_constants(1500, 200)
-        self.motor3.set_iq_pid_constants(1500, 200)
         self.motor3.set_speed_pid_constants(4e-2, 4e-4, 3e-2)
         self.motor3.set_position_pid_constants(275, 0, 0)
         self.motor3.set_position_region_boundary(250000)
@@ -205,7 +202,6 @@ class MotorThread(threading.Thread):
         self.motor4 = PowerfulBLDCDriver(self.i2c, 25)
         self.motor4.set_current_limit_foc(262144)
         self.motor4.set_id_pid_constants(1500, 200)
-        self.motor4.set_iq_pid_constants(1500, 200)
         self.motor4.set_speed_pid_constants(4e-2, 4e-4, 3e-2)
         self.motor4.set_position_pid_constants(275, 0, 0)
         self.motor4.set_position_region_boundary(250000)
