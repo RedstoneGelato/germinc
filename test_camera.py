f965ff163a1beca9184121612eb573ce88a210ab
RedstoneGelato <wanjerold@gmail.com>
2026-09-02 13:15:49 +1000

    imshow rah

--- test_camera.py
+++ test_camera.py
@@ -148,6 +148,7 @@ def main():
             if display_available:
                 try:
                     cv2.imshow("camera debug", annotated)
+                    cv2.imshow("orange", orange_mask)
                     if cv2.waitKey(1) & 0xFF == ord('q'):
                         break
                 except cv2.error:
