c94631f72d0a93092241ee88c0c038b675a820e0
RedstoneGelato <wanjerold@gmail.com>
2026-09-03 18:53:34 +1000

    mayb

--- 1defense.py
+++ 1defense.py
@@ -747,7 +747,7 @@ def main():
                     desired_pos = [0,0]
 
             elif botstate == 1: # go for ball then score
-                if ballpos[1] < 60 and ballpos[1] > 0 and abs(ballpos[0]) < 50:
+                if ballpos[1] < 50 and ballpos[1] > 0 and abs(ballpos[0]) < 50:
                     raw_substate1 = 1  # ball in bcz
                 elif ballpos[1] < 60:
                     raw_substate1 = 2 if ballpos[1] < -90 else 3  # far vs near backup
@@ -763,8 +763,8 @@ def main():
                     desired_pos = ballpos
                 elif substate1 == 3:
                     desired_heading = 0
-                    if abs(ballpos[0]) < 80:
-                        desired_pos = [-200, 0] if goalpos[0] < 90 or own_goalpos[0] < 90 else [200, 0]
+                    if abs(ballpos[0]) < 50:
+                        desired_pos = [-200, 0] if goalpos[0] < 60 or own_goalpos[0] < 60 else [200, 0]
                     else:
                         desired_pos = [0, -200]
                 elif substate1 == 4:
@@ -776,7 +776,7 @@ def main():
                     desired_pos = [0,200]
                     desired_heading = 0
                 else:
-                    if ballpos[1] < 70 and ballpos[1] > 0 and abs(ballpos[0]) < 50:
+                    if ballpos[1] < 50 and ballpos[1] > 0 and abs(ballpos[0]) < 50:
                         raw_substate2 = 1  # ball in bcz
                     elif ballpos[1] < 60:
                         raw_substate2 = 2 if ballpos[1] < -90 else 3  # far vs near backup
@@ -792,8 +792,8 @@ def main():
                         desired_pos = [0, -200]
                     elif substate2 == 3:
                         desired_heading = 0
-                        if abs(ballpos[0]) < 80:
-                            desired_pos = [-200, 0] if goalpos[0] < 90  or own_goalpos[0] < 90 else [200, 0]
+                        if abs(ballpos[0]) < 50:
+                            desired_pos = [-200, 0] if goalpos[0] < 60 or own_goalpos[0] < 60 else [200, 0]
                         else:
                             desired_pos = [0, -200]
                     elif substate2 == 4:
