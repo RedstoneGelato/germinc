a88e762395e4915022e67ab21608eaaa0593772d
RedstoneGelato <wanjerold@gmail.com>
2026-09-10 19:55:30 +1000

    chill bro

--- 1defense.py
+++ 1defense.py
@@ -818,7 +818,7 @@ def main():
             elif botstate == 1: #go for ball then score
                 if (ball_distance < 170 and ballpos[1] > 0 and abs(ballpos[0]) < 60 and ir_snapshot[0].get("distance") > 2) or ir_snapshot[0].get("distance") == 4:
                     raw_substate1 = 1  #ball in bcz
-                elif ballpos[1] < 20:
+                elif ballpos[1] < 60:
                     raw_substate1 = 2 if ball_distance > 200 else 3  #far vs near backup #TUNE: 200 to be far
                 else:
                     raw_substate1 = 4  #pathfind to ball
